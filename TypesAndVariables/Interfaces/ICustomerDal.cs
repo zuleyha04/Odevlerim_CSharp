d87cb98ed6c759efe0b56bcd2e2204b14151b0c0
zuleyha04 <zuleyhakbas04@gmail.com>
2024-02-24 13:30:04 +0300

    CSharpCourse_Basic_
    Engin Demiroğ'un oynatma listesi çalışılarak oluşturulan temel seviye C# ödevim.
    2. Hafta 1. Ödev

---
+++ TypesAndVariables/Interfaces/ICustomerDal.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    interface ICustomerDal
+    {
+        void Add();
+        void Update();
+        void Delete();
+    }
+
+    class SqlServerCustomerDal : ICustomerDal
+    {
+        void ICustomerDal.Add()
+        {
+            Console.WriteLine("Sql added");
+        }
+
+        void ICustomerDal.Delete()
+        {
+            Console.WriteLine("Sql deleted");
+        }
+
+        void ICustomerDal.Update()
+        {
+            Console.WriteLine("Sql updated");
+        }
+    }
+    class MySqlCustomerDal : ICustomerDal
+    {
+        void ICustomerDal.Add()
+        {
+            Console.WriteLine("MySql added");
+        }
+
+        void ICustomerDal.Delete()
+        {
+            Console.WriteLine("MySql deleted");
+        }
+
+        void ICustomerDal.Update()
+        {
+            Console.WriteLine("MySql updated");
+        }
+    }
+
+    class OracleCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Oracle added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Oracle deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Oracle updated");
+        }
+    }
+
+    class CustomerManager
+    {
+        public void Add(ICustomerDal customerDal)//böylece sistem bir veri tabanına bağlı kalmamış oldu interface'e bağlı
+        {
+            customerDal.Add();
+        }
+    }
+
+}
