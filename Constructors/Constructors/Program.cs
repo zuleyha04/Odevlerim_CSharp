1032e4727139ef1b1bfa4f753e26da69b6477eb1
zuleyha04 <zuleyhakbas04@gmail.com>
2024-02-26 21:49:45 +0300

    Constructor
    constructor'ın çalışması

---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { Id = 1,FirstName="Ayşe",LastName="Dikmen",City="İstanbul" };
+
+            Customer customer2=new Customer(2,"Derya","Demir","Ankara");
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+
+
+            Console.WriteLine(customer2.FirstName);
+        }
+    }
+
+    class Customer
+    {
+
+        public Customer()
+        {
+            
+        }
+
+        //default constructors
+        public Customer(int id,string firstName,string lastName,string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+
+        public int Id{ get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string City { get; set; }
+    }
+
+}
